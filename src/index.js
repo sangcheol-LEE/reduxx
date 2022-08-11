acafe2af0ed3db53b613de900dfcbc9267a11b56
이상철 <zlrz6706@gmail.com>
2022-08-11 23:01:54 +0900

    복습 복습 복습 복습 복습

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { createStore } from 'redux';
-import rootReducer from './modules';
-import { Provider } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-const store = createStore(rootReducer, composeWithDevTools())
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
     <App />
-  </Provider>
 );
