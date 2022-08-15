d5606e83a0f9d411fe401dc94f93f760f1f96aaa
이상철 <zlrz6706@gmail.com>
2022-08-15 16:28:16 +0900

    다시 반복

--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from './modules';
-import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer, composeWithDevTools());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <Provider store={store}>
         <App />
-    </Provider>
 );
